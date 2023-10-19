dc5b0e1cd2d83d82664ab74311e170bc150d627b
Kevin Vicente <kcvicente@utpl.edu.ec>
2023-10-19 16:52:48 -0500

    segundo commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -15,6 +15,10 @@ public class Proyecto01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+        System.out.println("Ejemplo de Java manejado desde GitHub Versionado a traves de Git creado por kevin162002");
+        System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub creado por kevin162022");
+        
     }
     
 }
